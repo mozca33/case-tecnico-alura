1876b7e1a9e15146aef43d871b172a018d08f1fa
Rafael Felipe <rafaelfelipe501@gmail.com>
2025-05-18 15:35:46 -0300

    refactor(course): moved controller to new package and added @Min(1) annotation to avoid negative id values

--- src/main/java/br/com/alura/AluraFake/course/controller/CourseController.java
+++ src/main/java/br/com/alura/AluraFake/course/controller/CourseController.java
@@ -1,14 +1,16 @@
-package br.com.alura.AluraFake.course;
+package br.com.alura.AluraFake.course.controller;
 
 import br.com.alura.AluraFake.course.dto.CourseDTO;
+import br.com.alura.AluraFake.course.dto.CourseListItemDTO;
 import br.com.alura.AluraFake.course.mapper.CourseMapper;
+import br.com.alura.AluraFake.course.service.CourseService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/course")
@@ -43,20 +44,13 @@ public class CourseController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CourseDTO> getCourseById(@PathVariable Long id) {
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable @Min(1) Long id) {
         return ResponseEntity.ok().body(courseMapper.toDTO(courseService.getById(id)));
     }
 
     @PostMapping("/{id}/publish")
-    public ResponseEntity<CourseDTO> publishCourse(@PathVariable Long id) {
+    public ResponseEntity<CourseDTO> publishCourse(@PathVariable @Min(1) Long id) {
         return ResponseEntity.ok().body(courseMapper.toDTO(courseService.publishCourse(id)));
-    }
-
-    @PutMapping("/{id}")
-    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
-        // TODO: process PUT request
-
-        return entity;
     }
 
 }
